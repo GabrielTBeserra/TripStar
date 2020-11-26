3a16280733a0dd56a4249e44a62f839e4ea44483
Gabriel Teles Beserra <gbeserra@unaerp.br>
2020-11-26 08:51:00 -0300

    Adicionado background animado

---
+++ Assets/Scripts/Controllers/Hud/BackgroundController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    [SerializeField]
+    public float XSpeed = 5;
+    [SerializeField]
+    public float YSpeed = 5;
+
+
+    Material material;
+    Vector2 offset;
+
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        offset = new Vector2(XSpeed, YSpeed);
+    }
+
+    void Update()
+    {
+
+        material.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
