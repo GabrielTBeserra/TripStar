3991c5443710ae06a443ecd25740d38404a340fe
Fernando Nagase <nagase.fernando@gmail.com>
2020-12-08 19:31:53 -0300

    Implementar pausa do jogo

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    private bool isPaused;
+
+    [SerializeField]
+    private GameObject pausePanel = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    private void Pause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0.0f : 1.0f;
+        pausePanel.SetActive(isPaused);
+    }
+}
