8fe83c8f8ecfc5ef2fb5215c8c7733dd4489e31e
Fernando Nagase <nagase.fernando@gmail.com>
2020-12-08 22:50:02 -0300

    Implementar mixing de audio

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioMixer mixer;
+
+    public void SetMasterVolume(float value)
+    {
+        mixer.SetFloat("masterVol", value);
+        print(value);
+    }
+
+    public void SetBgmVolume(float value)
+    {
+        mixer.SetFloat("musicVol", value);
+    }
+}
