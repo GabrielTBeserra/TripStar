9cdf59fd0a64f2fe47dea44b96de3d44671ce80d
Fernando Nagase <nagase.fernando@gmail.com>
2020-12-08 21:31:04 -0300

    Implementar interface para gerenciar UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Text scoreText = null;
+    [SerializeField] private GameObject lifeGameObject = null;
+
+    public void UpdateScore(int score)
+    {
+        scoreText.text = $"Pontuação: {score}";
+    }
+}
