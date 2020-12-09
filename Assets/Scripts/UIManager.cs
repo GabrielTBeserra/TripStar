1f92cfaf4a233a3849807bd733c06d1629061bda
Fernando Nagase <nagase.fernando@gmail.com>
2020-12-09 18:42:22 -0300

    Prover interface para atualizar vida na HUD

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,9 +5,17 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] private Text scoreText = null;
     [SerializeField] private GameObject lifeGameObject = null;
+    [SerializeField] private Image lifebarFill = null;
+
+    [SerializeField] private int maxHealth = 3;
 
     public void UpdateScore(int score)
     {
         scoreText.text = $"Pontuação: {score}";
+    }
+
+    public void UpdateHealth(int health)
+    {
+        lifebarFill.fillAmount = (float)health / maxHealth;
     }
 }
